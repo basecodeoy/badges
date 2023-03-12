a84ffd243b00d9cc8e6b0be41e331e93cfc0e9c3
Brian Faust <hello@basecode.sh>
2023-03-12 06:01:35 +0200

    ci: update configurations

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,22 +4,16 @@ declare(strict_types=1);
 
 namespace PreemStudio\Badges;
 
-use Spatie\LaravelPackageTools\Commands\InstallCommand;
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use PreemStudio\Jetpack\Package\AbstractServiceProvider;
+use PreemStudio\Jetpack\Package\Package;
 
-final class ServiceProvider extends PackageServiceProvider
+final class ServiceProvider extends AbstractServiceProvider
 {
     public function configurePackage(Package $package): void
     {
         $package
             ->name('laravel-badges')
             ->hasConfigFile('laravel-badges')
-            ->hasMigration('create_badges_tables')
-            ->hasInstallCommand(function (InstallCommand $command) {
-                $command
-                    ->publishConfigFile()
-                    ->publishMigrations();
-            });
+            ->hasMigration('create_badges_tables');
     }
 }
