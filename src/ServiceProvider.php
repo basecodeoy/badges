e55367eaa8f0e127f40f08d005e0fa8d27863d82
Brian Faust <brian@faust.de.com>
2024-11-04 13:28:03 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BaseCodeOy\Badges;
+
+use BaseCodeOy\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    //
+}
